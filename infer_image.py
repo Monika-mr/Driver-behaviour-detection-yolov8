821aa0ba6436c8e7e700a3966064579c07d04f0a
Monika M R <125423534+Monika-mr@users.noreply.github.com>
2026-03-23 12:15:43 +0530

    Update infer_image.py

--- infer_image.py
+++ infer_image.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-model = YOLO(r"C:\Users\mrmon\Downloads\Annotation\annotation 2\Monika\monica.v1i.yolov5pytorch\runs\detect\train3\weights\best.pt")
-model.predict(r"C:\Users\mrmon\Downloads\Annotation\annotation 2\Monika\monica.v1i.yolov5pytorch\train\images\frame_1007_jpg.rf.1585c669db0dec7b527ebfcfb1cff116.jpg", show=True)
+model = YOLO("best.pt")
+model.predict("test.jpg", show=True)
