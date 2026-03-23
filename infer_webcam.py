c956f7bee256993ad17886f26c5edc090c6a08b9
Monika M R <125423534+Monika-mr@users.noreply.github.com>
2026-03-23 12:11:31 +0530

    Create infer_webcam.py

---
+++ infer_webcam.py
@@ -0,0 +1,16 @@
+import cv2
+from ultralytics import YOLO
+model = YOLO("runs/detect/train3/weights/best.pt")
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read() 
+    if not ret:
+        break
+    results = model(frame)
+    annotated = results[0].plot()
+    cv2.imshow("Driver Monitoring", annotated)
+    # ⭐ CLEAN EXIT
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
